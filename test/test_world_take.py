2a609863113c4a60a2d1906fbd95d8fdd4b5ed54
Jeffrey Elliott <Jeffrey.elliott@gmail.com>
2026-07-05 10:15:37 -0700

    Add items in inventory out of scope for take test

--- test/test_world_take.py
+++ test/test_world_take.py
@@ -161,3 +161,28 @@ def test_take_same_named_item_in_other_room_does_not_create_ambiguity():
     assert hidden_coin.id in other_room.component("container").items
     assert hidden_coin.id not in player_entity.component("container").items
     assert hidden_coin.component("containable").parent == other_room.id
+    
+    
+def test_take_ignores_same_named_inventory_item_after_prior_take():
+    world = World()
+
+    room_entity = room(world, "room")
+    player_entity = player(world)
+    red_coin = coin_with_trait(world, "red")
+    blue_coin = coin_with_trait(world, "blue")
+
+    world.current = room_entity.id
+    world.player_id = player_entity.id
+    world.contain(room_entity, red_coin)
+    world.contain(room_entity, blue_coin)
+
+    first_result = world.handle("take red coin")
+    second_result = world.handle("take coin")
+
+    assert first_result.ok
+    assert second_result.ok
+
+    assert red_coin.id in player_entity.component("container").items
+    assert blue_coin.id in player_entity.component("container").items
+    assert red_coin.component("containable").parent == player_entity.id
+    assert blue_coin.component("containable").parent == player_entity.id
